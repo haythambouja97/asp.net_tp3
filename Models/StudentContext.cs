09354e4ac7276d9981868d4b90631288e0c79ff2
haythambouja97 <101941931+haythambouja97@users.noreply.github.com>
2022-05-05 15:14:24 +0100

    file upload

---
+++ Models/StudentContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class StudentContext : DbContext
+{
+    public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+    {
+    }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<School> Schools { get; set; }
+}
